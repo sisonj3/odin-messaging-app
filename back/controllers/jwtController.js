8a3a9612a29b8bd10c530238fab50d225d1c025c
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-12-02 17:23:04 -0800

    Created get and verify JWT

---
+++ back/controllers/jwtController.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv").config();
+
+// Get JSON Web Token
+const getJWT = (req, res) => {
+    // Get user back w/ req.user
+
+    // Get jsonwebtoken
+    jwt.sign({ user: req.user }, process.env.SECRET, { expiresIn: '12h' }, (err, token) => {
+        res.json({
+            token: token,
+            id: req.user.id,
+            username: req.user.username,
+        }); 
+    });
+};
+
+// Verify JSON Web Token
+const verifyToken = (req, res, next) => {
+    // Get auth header value
+    const bearerHeader = req.headers['Authorization'];
+
+    // Check if bearer is undefined
+    if (bearerHeader) {
+        const bearer = bearerHeader.split(' ');
+
+        // Get token from arrray
+        const bearerToken = bearer[1];
+
+        // Set the token
+        req.token = bearerToken;
+
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+};
+
+module.exports = {
+    getJWT,
+    verifyToken,
+};
