6a7bd2cad7df7efaa3f96c42efd309edcbdef5f3
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-12-04 15:50:36 -0800

    Obtain JWT from back end through login page

---
+++ front/src/components/Login.jsx
@@ -0,0 +1,48 @@
+function Login({ parentGetJWT }) {
+
+    const returnJWT = (event) => {
+
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+
+        // Fetch login to get JWT
+        fetch('http://localhost:3000/login', {
+            mode: 'cors',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: username, password: password}),
+        })
+        .then(response => response.json())
+        .then(response => {
+            // console.log(response);
+            parentGetJWT(response.token);
+        })
+        .catch(error => console.error(error));
+
+        event.preventDefault();
+    };
+
+    return (
+        <>
+            <form onSubmit={returnJWT}>
+                <div>
+                    <label htmlFor="username">
+                        Username <input type="text" name="username" id="username" />
+                    </label>
+                </div>
+                
+                <div>
+                    <label htmlFor="password">
+                        Password <input type="password" name="password" id="password" />
+                    </label>
+                </div>
+                
+                <button type="submit">Log In</button>
+            </form>
+        </>
+    )
+}
+
+export default Login;
