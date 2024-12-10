15e4f0c0be4a4d47cfe5fddbda992cbe18ce4a5e
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-12-09 17:27:30 -0800

    Added 'authorization' to allowed headers

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, authorization');
 
     next();
 });
